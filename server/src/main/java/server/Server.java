5f1facad3feab61a0b431310ca4e950f0f5036b1
searlejk <jeremykimsearle@gmail.com>
2025-02-26 13:32:46 -0700

    built framework for server file

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -10,6 +10,33 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.post("/user", (req, res) -> {
+            return "post user";
+        });
+
+        Spark.post("/session", (req, res) -> {
+            return "post session";
+        });
+
+        Spark.delete("/session", (req, res) -> {
+            return "delete session";
+        });
+
+        Spark.get("/game", (req, res) -> {
+            return "get game";
+        });
+
+        Spark.post("/game", (req, res) -> {
+            return "post game";
+        });
+
+        Spark.put("/game", (req, res) -> {
+            return "put game";
+        });
+
+        Spark.delete("/db", (req, res) -> {
+            return "delete database (clear)";
+        });
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
