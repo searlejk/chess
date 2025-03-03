32007efe10907473261e0c8f20e65170a1e4a794
searlejk <jeremykimsearle@gmail.com>
2025-03-02 18:22:17 -0700

    register almost works

--- server/src/main/java/dataaccess/MemoryDataAccess.java
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -9,14 +9,14 @@ import java.util.HashMap;
 
 public class MemoryDataAccess implements DataAccess {
     private int nextId = 1;
-    final private HashMap<Integer, UserData> Users = new HashMap<>();
+    final private HashMap<String, UserData> Users = new HashMap<>();
     final private HashMap<Integer, GameData> Games = new HashMap<>();
     final private HashMap<String, AuthData> authDatas = new HashMap<String, AuthData>();
 
     public UserData addUserData(UserData user) {
         user = new UserData(user.username(), user.password(),user.email());
 
-        Users.put(Integer.valueOf(user.username()), user);
+        Users.put(user.username(), user);
         return user;
     }
 
@@ -43,7 +43,7 @@ public class MemoryDataAccess implements DataAccess {
     public UserData addUser(UserData user) {
         user = new UserData(user.username(), user.password(),user.email());
 
-        Users.put(Integer.valueOf(user.username()), user);
+        Users.put(user.username(), user);
         return user;
     }
 
@@ -54,13 +54,5 @@ public class MemoryDataAccess implements DataAccess {
     public UserData getUser(String username) {
         return Users.get(username);
     }
-
-//    public void deleteUserData(Integer id) {
-//        Users.remove(id);
-//    }
-//
-//    public void deleteAllUserData() {
-//        Users.clear();
-//    }
 
 }
---
+++ server/src/test/java/service/RegisterTest.java
@@ -0,0 +1,23 @@
+//package service;
+//
+//import dataaccess.DataAccess;
+//import dataaccess.MemoryDataAccess;
+//import model.RegisterRequest;
+//import model.RegisterResponse;
+//import service.UserService;
+//import org.junit.jupiter.api.Test;
+//
+//public class RegisterTest {
+//
+//    @Test
+//    void RegisterPositiveTest() {
+//        RegisterRequest req = new RegisterRequest("username","password","email");
+//        RegisterResponse temp = UserService.register(req);
+//        UserService.
+//    }
+//
+//    @Test
+//    void RegisterNegativeTest() {
+//        DataAccess dataAccess = new MemoryDataAccess();
+//    }
+//}
