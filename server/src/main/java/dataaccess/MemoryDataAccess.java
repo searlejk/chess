4872d5aeb009bb87211f3627e29e187b4cdb17bc
searlejk <jeremykimsearle@gmail.com>
2025-03-03 22:49:38 -0700

    now starting LocalVariableName

--- server/src/main/java/dataaccess/MemoryDataAccess.java
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -1,26 +1,20 @@
 package dataaccess;
 
-import chess.ChessGame;
 import model.AuthData;
 import model.GameData;
 import model.UserData;
 
-import java.math.BigInteger;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Random;
-import java.util.UUID;
-import java.util.random.*;
-
-import static org.eclipse.jetty.util.LazyList.size;
 
 public class MemoryDataAccess implements DataAccess {
-    final private HashMap<String, UserData> Users = new HashMap<>();
-    final private HashMap<Integer, GameData> Games = new HashMap<>();
+    final private HashMap<String, UserData> users = new HashMap<>();
+    final private HashMap<Integer, GameData> games = new HashMap<>();
     final private HashMap<String, AuthData> authData = new HashMap<>();
 
     public Collection<UserData> listUserDatas() {
-        return Users.values();
+        return users.values();
     }
 
     public Collection<AuthData> listAuthDatas() {
@@ -30,8 +24,8 @@ public class MemoryDataAccess implements DataAccess {
     public UserData addUser(UserData user) {
         user = new UserData(user.username(), user.password(),user.email());
 
-        Users.put(user.username(), user);
-        System.out.println(Users.values());
+        users.put(user.username(), user);
+        System.out.println(users.values());
         return user;
     }
 
@@ -40,13 +34,13 @@ public class MemoryDataAccess implements DataAccess {
     }
 
     public UserData getUser(String username) {
-        return Users.get(username);
+        return users.get(username);
     }
 
     public UserData getUserByAuth(String authToken){
         String username = authData.get(authToken).username();
         if (username!=null) {
-            return Users.get(username);
+            return users.get(username);
         }
         return null;
     }
@@ -56,7 +50,7 @@ public class MemoryDataAccess implements DataAccess {
     }
 
     public Collection<GameData> listGames(){
-        return Games.values();
+        return games.values();
     }
 
     public GameData addGame(Integer inputGameID, GameData gameData) {
@@ -68,25 +62,25 @@ public class MemoryDataAccess implements DataAccess {
         }
         GameData newGameData = new GameData(gameID,gameData.whiteUsername(), gameData.blackUsername(), gameData.gameName());
         System.out.println("Added game " + gameID + ": " + newGameData.gameName());
-        Games.put(gameID,newGameData);
-        System.out.println(Games.values());
+        games.put(gameID,newGameData);
+        System.out.println(games.values());
         return newGameData;
     }
 
     public GameData getGame(int gameID){
-        return Games.get(gameID);
+        return games.get(gameID);
     }
 
     public void remGame(int gameID){
-        Games.remove(gameID);
+        games.remove(gameID);
     }
 
     public void clearUsersAndAuth() {
         authData.clear();
-        Users.clear();
+        users.clear();
     }
 
     public void clearGames() {
-        Games.clear();
+        games.clear();
     }
 }
