0d952921dda81e84edb3203d6eaeb183a9cb9c89
searlejk <jeremykimsearle@gmail.com>
2025-03-04 18:32:00 -0700

    New and Extra Improved PawnMovesCalculator

--- shared/src/main/java/chess/PawnMovesCalculator.java
+++ shared/src/main/java/chess/PawnMovesCalculator.java
@@ -24,7 +24,6 @@ public class PawnMovesCalculator {
         this.pos = pos;
     }
 
-
     public Collection<ChessMove> legalMoves(ChessBoard board, ChessPosition myPos) {
         ChessPiece piece = board.getPiece(myPos);
         ChessGame.TeamColor color = piece.getTeamColor();
@@ -43,8 +42,6 @@ public class PawnMovesCalculator {
             pawnRowOffsets = new int[]{ -1, -1, -1, -2 };
             pawnColOffsets = new int[]{ -1,  0,  1,  0 };
         }
-
-
 
         movesCopy = MoveCalcHelper.getInstance().calcLegalMovesFromAllPositions(
                 pawnRowOffsets,
@@ -65,84 +62,34 @@ public class PawnMovesCalculator {
             Collection<ChessPiece.PieceType> promoList = List.of(QUEEN, KNIGHT, BISHOP, ROOK);
 
             // diagonal move with no piece to capture
-            if (endCol!=col && endPiece==null){
-                legalMoves.remove(move);
-                continue;
-            }
+            if (endCol!=col && endPiece==null){ legalMoves.remove(move); continue; }
 
             // forward single move with piece blocking way
-            if (endPiece!=null && endCol==col){
-                legalMoves.remove(move);
-                continue;
-            }
+            if (endPiece!=null && endCol==col){ legalMoves.remove(move); continue; }
 
             // forward double illegal start position
-            if ((endRow==row+2 && row!=2) ||
-                    endRow==row-2 && row!=7){
-                legalMoves.remove(move);
-                continue;
-            }
+            if ((endRow==row+2 && row!=2) || endRow==row-2 && row!=7){ legalMoves.remove(move); continue; }
 
-            // WHITE diagonal promotion update
-            if (endRow==8 && color==WHITE && endCol!=col){
+            // all promotion updates
+            if ((endRow==8 && color==WHITE) || (endRow==1 && color==BLACK)){
                 legalMoves.remove(move);
                 for (ChessPiece.PieceType promo : promoList ){
                     ChessMove promoMove = new ChessMove(myPos,endPos,promo);
                     legalMoves.add(promoMove);
-                }
-                continue;
+                } continue;
             }
-
-            // WHITE straight promotion update
-            if (endRow==8 && color==WHITE){
-                legalMoves.remove(move);
-                for (ChessPiece.PieceType promo : promoList ){
-                    ChessMove promoMove = new ChessMove(myPos,endPos,promo);
-                    legalMoves.add(promoMove);
-                }
-                continue;
-            }
-
-            // BLACK diagonal promotion update
-            if (endRow==1 && color==BLACK && endCol!=col){
-                legalMoves.remove(move);
-                for (ChessPiece.PieceType promo : promoList ){
-                    ChessMove promoMove = new ChessMove(myPos,endPos,promo);
-                    legalMoves.add(promoMove);
-                }
-                continue;
-            }
-
-            // BLACK straight promotion update
-            if (endRow==1 && color==BLACK){
-                legalMoves.remove(move);
-                for (ChessPiece.PieceType promo : promoList ){
-                    ChessMove promoMove = new ChessMove(myPos,endPos,promo);
-                    legalMoves.add(promoMove);
-                }
-                continue;
-            }
-
 
             // forward double WHITE move
             ChessPosition midWhitePos = new ChessPosition(row+1,col);
             ChessPiece midWhitePiece = board.getPiece(midWhitePos);
-            if (endRow==row+2 && midWhitePiece!=null){
-                legalMoves.remove(move);
-            }
+            if (endRow==row+2 && midWhitePiece!=null)   { legalMoves.remove(move);}
 
             // forward double BLACK move
             ChessPosition midBlackPos = new ChessPosition(row-1,col);
             ChessPiece midBlackPiece = board.getPiece(midBlackPos);
-            if (endRow==row-2 && midBlackPiece!=null){
-                legalMoves.remove(move);
-            }
-
-
+            if (endRow==row-2 && midBlackPiece!=null)   { legalMoves.remove(move); }
 
         }
-
         return legalMoves;
     }
 }
-
